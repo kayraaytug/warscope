86b824328afd11f92b670a6a80443624e12ce391
Kayra Aytug <kayraaytug@outlook.com>
2024-10-04 14:08:05 +0300

    ai graphs will be added

--- lib/mapscreen.dart
+++ lib/mapscreen.dart
@@ -19,58 +19,88 @@ class _MapScreenState extends State<MapScreen> {
   final LayerHitNotifier<HitValue> _hitNotifier = ValueNotifier(null);
   List<HitValue>? _prevHitValues;
   List<Polygon<HitValue>>? _hoverGons;
-List<Icon> iconList = [
-  Icon(Icons.airplane_ticket),
-  Icon(Icons.local_gas_station),
-  Icon(Icons.question_answer),
-  Icon(Icons.attach_money),
-  Icon(Icons.question_answer),
-  Icon(Icons.water),
-  Icon(Icons.cleaning_services),
-  Icon(Icons.school),
-  Icon(Icons.fastfood),
-  Icon(Icons.work),
-  Icon(Icons.health_and_safety),
-  Icon(Icons.electrical_services),
-  Icon(Icons.warning),
-  Icon(Icons.people_alt),
-  Icon(Icons.psychology),
-  Icon(Icons.group),
+  List<Icon> iconList = [
+    Icon(Icons.airplane_ticket),
+    Icon(Icons.local_gas_station),
+    Icon(Icons.question_answer),
+    Icon(Icons.attach_money),
+    Icon(Icons.question_answer),
+    Icon(Icons.water),
+    Icon(Icons.cleaning_services),
+    Icon(Icons.school),
+    Icon(Icons.fastfood),
+    Icon(Icons.work),
+    Icon(Icons.health_and_safety),
+    Icon(Icons.electrical_services),
+    Icon(Icons.warning),
+    Icon(Icons.people_alt),
     Icon(Icons.psychology),
-  Icon(Icons.group),
-];
+    Icon(Icons.group),
+    Icon(Icons.psychology),
+    Icon(Icons.group),
+  ];
 
   final _polygonsRaw = <Polygon<HitValue>>[
     Polygon(
-      points: palestine_1,
+      points: ukrainePolygon,
       color: Colors.green.withOpacity(0.4),
       borderStrokeWidth: 2,
       borderColor: Colors.blue,
       hitValue: (
-        title: 'Israel-Palestine Conflict',
+        title: 'Yemen',
         subtitle: '',
       ),
     ),
     Polygon(
-      points: palestine_2,
-      color: Colors.green.withOpacity(0.4),
+      points: syriaPolygon,
+      color: Colors.orange.withOpacity(0.4),
       borderStrokeWidth: 2,
       borderColor: Colors.blue,
       hitValue: (
-        title: 'Israel-Palestine Conflict',
+        title: 'Suriye',
         subtitle: '',
       ),
     ),
     Polygon(
-      points: sudan,
+      points: palestinePolygon,
       color: Colors.green.withOpacity(0.4),
       borderStrokeWidth: 2,
       borderColor: Colors.blue,
       hitValue: (
-        title: 'Sudan Civil War',
+        title: 'Filistin',
         subtitle: '',
       ),
-    )
+    ),
+    Polygon(
+      points: israelPolygon,
+      color: Colors.red.withOpacity(0.4),
+      borderStrokeWidth: 2,
+      borderColor: Colors.blue,
+      hitValue: (
+        title: 'İsrail',
+        subtitle: '',
+      ),
+    ),
+    Polygon(
+      points: iraqPolygon,
+      color: Colors.orange.withOpacity(0.4),
+      borderStrokeWidth: 2,
+      borderColor: Colors.blue,
+      hitValue: (
+        title: 'Irak',
+        subtitle: '',
+      ),
+    ),
+    Polygon(
+      points: yemenPolygon,
+      color: Colors.orange.withOpacity(0.4),
+      borderStrokeWidth: 2,
+      borderColor: Colors.blue,
+      hitValue: (
+        title: 'Yemen',
+        subtitle: '',
+      ),
+    ),
   ];
   late final _polygons =
       Map.fromEntries(_polygonsRaw.map((e) => MapEntry(e.hitValue, e)));
@@ -135,8 +165,8 @@ List<Icon> iconList = [
           FlutterMap(
             mapController: _mapController,
             options: const MapOptions(
-              initialCenter: LatLng(51.5, -0.09),
-              initialZoom: 5,
+              initialCenter: LatLng(33.0461, 36.8516),
+              initialZoom: 7,
             ),
             children: [
               TileLayer(
@@ -157,8 +187,15 @@ List<Icon> iconList = [
                         );
                       },
                       child: Container(
-                  child: Image.asset('assets/icon48_green.png'),
-                ),
+child: Image.asset(
+  ['Filistin', 'Ukrayna'].contains(entry.key)
+      ? 'assets/icon48_green.png' // Green icon for Yemen, Ukrayna
+      : ['Rusya', 'İsrail'].contains(entry.key)
+          ? 'assets/icon48_red.png' // Red icon for Rusya, İsrail
+          : ['Suriye', 'Irak', 'Yemen'].contains(entry.key)
+              ? 'assets/icon48_orange.png' // Orange icon for Suriye, Irak, Yemen
+              : 'assets/icon48_green.png', // Default icon if no match
+),                      ),
                     ),
                   );
                 }).toList(),
@@ -228,67 +265,126 @@ List<Icon> iconList = [
               ),
             ],
           ),
+          Positioned(
+  top: 10,
+  left: 10,
+  child: Card(
+    elevation: 5,
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 16,
+                height: 16,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.red, // Red for "Agresör"
+                ),
+              ),
+              SizedBox(width: 8), // Space between circle and text
+              Text('Agresör'),
+            ],
+          ),
+          SizedBox(height: 8), // Space between rows
+          Row(
+            children: [
+              Container(
+                width: 16,
+                height: 16,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.green, // Green for "Müdafaa"
+                ),
+              ),
+              SizedBox(width: 8), // Space between circle and text
+              Text('Müdafaa'),
+            ],
+          ),
+          SizedBox(height: 8), // Space between rows
+          Row(
+            children: [
+              Container(
+                width: 16,
+                height: 16,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.orange, // Orange for "İç Savaş"
+                ),
+              ),
+              SizedBox(width: 8), // Space between circle and text
+              Text('İç Savaş'),
+            ],
+          ),
+        ],
+      ),
+    ),
+  ),
+),
+
+
         ],
       ),
     );
   }
 
-void _openTouchedGonsModal(
-  String eventType,
-  List<HitValue> tappedLines,
-  LatLng coords,
-) {
+  void _openTouchedGonsModal(
+    String eventType,
+    List<HitValue> tappedLines,
+    LatLng coords,
+  ) {
+    // Extract the conflict title from the tapped polygon
+    final tappedConflict = tappedLines.first.title;
+    final conflictData = warData[tappedConflict];
 
-
-  // Extract the conflict title from the tapped polygon
-  final tappedConflict = tappedLines.first.title;
-  final conflictData = warData[tappedConflict];
-
-  showModalBottomSheet<void>(
-    context: context,
-    builder: (context) => Padding(
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            tappedConflict,
-            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 8),
-          if (conflictData != null)
-            Expanded(
-              child: ListView.builder(
-                itemCount: conflictData.length,
-                itemBuilder: (context, index) {
-                  final category = conflictData.keys.elementAt(index);
-                  final value = conflictData[category];
-                  return ListTile(
-                    leading: iconList[index],
-                    title: Text(
-                      category,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: Text(value.toString()),
-                    dense: true,
-                  );
-                },
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (context) => Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              tappedConflict,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            if (conflictData != null)
+              Expanded(
+                child: ListView.builder(
+                  itemCount: conflictData.length,
+                  itemBuilder: (context, index) {
+                    final category = conflictData.keys.elementAt(index);
+                    final value = conflictData[category];
+                    return ListTile(
+                      leading: iconList[index],
+                      title: Text(
+                        category,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(value.toString()),
+                      dense: true,
+                    );
+                  },
+                ),
+              ),
+            const SizedBox(height: 8),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: double.infinity,
+                child: OutlinedButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Close'),
+                ),
               ),
             ),
-          const SizedBox(height: 8),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: SizedBox(
-              width: double.infinity,
-              child: OutlinedButton(
-                onPressed: () => Navigator.pop(context),
-                child: const Text('Close'),
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
-    ),
-  );
+    );
   }
 }
